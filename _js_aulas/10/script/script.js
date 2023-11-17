0dd5a3d660b3146c3b391ab970110f78ed447460
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2023-11-17 19:02:08 -0300

    17-11 iniciando

--- _js_aulas/10/script/script.js
+++ _js_aulas/10/script/script.js
@@ -1,58 +1,37 @@
-// console.log("Ainda não executou!")
+console.log(document.body);
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1]);
+console.log(document.body.childNodes[1].childNodes[1]);
+console.log(document.body.childNodes[1].childNodes[1].textContent);
 
-// setTimeout(function () {
-//     console.log("Requisição assíncrona!")
-// }, 2000);
+// tag
 
-// console.log("Ainda não executou 2")
+const listItens = document.getElementsByTagName("li");
 
-// console.log("Ainda não!")
-
-// setInterval(function () {
-//     console.log("Requisição assíncrona!")
-// }, 3000);
-
-// console.log("Ainda não 2")
-
-// const promessa = Promise.resolve(5 + 10); 
-
-// console.log("Código ... blá blá ...")
-
-// promessa
-//     .then((value) => {
-//         console.log(`A soma é ${value}`);
-//         return value;
-//     })
-//     .then((value) => value +3)
-//     .then((value) => console.log(`Agora o valor é:  ${value}`));
-
-// Promise.resolve(4 * "adg") 
-//     .then ((n) => {
-//         if (Number.isNaN(n)) {
-//             throw new Error("Valores inválidos")
-//         }
-//     } )
-//     .catch((err) => console.log(`Um erro ocorreu: ${err}`));
-
-function checkNumber(n) {
-    return new Promise((resolve, reject) => {
-        if (n > 10){
-            resolve(`O numero é maior que 10!`);
-        } else {
-            reject(new Error("Numero abaixo do esperado"));
-        }
-    });
-}
-
-const a = checkNumber(20);
-const b = checkNumber(5);
+console.log(listItens); 
 
 
-a.then((v) => console.log(`O resultado é: ${v}`)).catch((err) =>
-  console.log(`Um erro ocorreu: ${err}`)
-);
+// Classe
+const products = document.getElementsByClassName("product");
+
+console.log(products); 
+
+// ID
+
+const title = document.getElementById("title"); 
+console.log(title); 
 
 
-b.then((v) => console.log(`O resultado é: ${v}`)).catch((err) =>
-  console.log(`Um erro ocorreu: ${err}`)
-);
+const productsQuery = document.querySelectorAll(".product");
+
+console.log(productsQuery);
+
+const mainContainer = document.querySelector("#main-container");
+
+console.log(mainContainer); 
+
+const p = document.createElement("p");
+
+const header = title.parentElement;
+
+header.insertBefore(p, title)
